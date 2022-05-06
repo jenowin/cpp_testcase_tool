878ff65f1b7794ba9336bfc45dc1ef5c288a1413
jenowin <jmcprogramer@gmail.com>
2022-05-06 14:12:43 +0530

    tweaks towards crossplatform

--- src/init.c
+++ src/init.c
@@ -7,8 +7,16 @@
 char CC[2] = "14";
 void file_COMPILE(const int* ARG_cnt, const char* test, const char* fname){
   char commands[380];
+
+#ifdef _WIN32
   sprintf(commands, "g++ -std=c++%s -O2 -g -Wall -Wshadow \"-Wl,--stack=268435456\" -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-unused-result -Wno-sign-conversion  -fno-sanitize-recover=all -fstack-protector-all -D FORTIFY_SOURCE=2  -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -o %s %s.cpp", CC, fname, fname);
+#elif __linux__
+  printf("Linux!!\n");
+  sprintf(commands, "g++ -std=c++%s -o %s %s.cpp", CC, fname, fname);
+#endif
+
   printf("Compiling %s.cpp with c++%s.\n", fname, CC);
+
   test ? printf("Running test - %s\n", test) : 0;
   if (!system(commands)) {
     COLOR_green(), EXIT_CODE_ZERO();
@@ -21,27 +29,43 @@ void file_COMPILE(const int* ARG_cnt, const char* test, const char* fname){
   return;
 }
 
-void UPDATE_OUTP(const char* fname, const char* IN, const char* OP) {
+void UPDATE_OUTP(const char* exec_fname, const char* IN, const char* OP) {
   char cmd[20];
-  sprintf(cmd, "%s<%s>%s", fname, IN, OP);
+  char new_l[20];
+  //sprintf(cmd, "%s<%s>%s", fname, IN, OP);
+
+#ifdef _WIN32
+  sprintf(cmd, "%s<%s>%s", exec_fname, IN, OP);
+  system(cmd);
+
+  //(echo.) >> output,  append new line(windows) at the end of file.
+  sprintf(new_l, "(echo.) >> %s", OP);
+  system(new_l);
+  return;
+#elif __linux__
+  //./%s<%s>%s No new line created.
+  // echo $(./%s<%s) > output, creates new line(linux).
+  sprintf(cmd, "echo $(./%s<%s) > %s", exec_fname, IN, OP);
   system(cmd);
   return;
+#endif
+
 }
 
-void DISPLAY_EXPECTED_OUTPUT(const char* in, const char* op, const char* exp_op) {
+void DISPLAY_EXPECTED_OUTPUT(const char* op, const char* exp_op) {
   char cmd[15];
 
   printf("Output:\n");
-  sprintf(cmd, "type %s", op);
+  sprintf(cmd, "cat %s", op);
   system(cmd);
   printf("\n____________________________\n");
 
   memset(cmd, 0, sizeof cmd);
 
   printf("Expected:\n");
-  sprintf(cmd, "type %s", exp_op);
+  sprintf(cmd, "cat %s", exp_op);
   system(cmd);
-  printf("____________________________\n");
+  printf("\n____________________________\n");
   return;
 }
 
@@ -61,7 +85,7 @@ int main (int argc, char* argv[]) {
 
   switch(argc) {
   case 1:
-    printf("No arguments argued!");
+    printf("No arguments argued!\n");
     break;
 
   case 2:
@@ -77,10 +101,15 @@ int main (int argc, char* argv[]) {
     char in_f[5] = "in", op_f[5] = "op";
     in_f[2] = *(argv[2]);
     op_f[2] = *(argv[2]);
+    !fopen(in_f, "r") ? fprintf(stderr, "Input file %s missing! \n", in_f), exit(EXIT_FAILURE) : 0;
+    !fopen(op_f, "r") ? fprintf(stderr, "Expected output file %s missing! \n", op_f), exit(EXIT_FAILURE) : 0;
 
     UPDATE_OUTP(argv[1], &in_f[0], "output");
-    __TRIM__(&argc, argv[2],  &in_f[0]);
-    DISPLAY_EXPECTED_OUTPUT(&in_f[0], "output", &op_f[0]);
+    __TRIM__(&in_f[0]);
+    __TRIM__(&op_f[0]);
+
+
+    DISPLAY_EXPECTED_OUTPUT("output", &op_f[0]);
     __CMP__(&op_f[0], "output");
     break;
   }
